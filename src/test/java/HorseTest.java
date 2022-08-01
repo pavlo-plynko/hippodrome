7c4f11b1862f47aeea6b05389b94bd48109cbbb1
JavaDen44 <dem.denis44@gmail.com>
2022-08-01 23:12:22 +0300

    Added branch dev

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
