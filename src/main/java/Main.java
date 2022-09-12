3200aebdbd0cb213e8e2a740505563f081c00159
Egor Melnichenko <e.melnichenko@erdc.ru>
2022-09-12 23:18:25 +0300

    add logs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -15,7 +19,9 @@ public class Main {
         );
         Hippodrome hippodrome = new Hippodrome(horses);
 
-        for (int i = 0; i < 100; i++) {
+        logger.info("Начало скачек. Количество участников: {}", horses.size());
+
+        for (int i = 0; i < 1; i++) {
             hippodrome.move();
             watch(hippodrome);
             TimeUnit.MILLISECONDS.sleep(200);
@@ -23,6 +29,8 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println("Победил " + winnerName + "!");
+
+        logger.info("Окончание скачек. Победитель: {}", winnerName);
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
