8ddcf9fd6ccf9a4184f8edd2404d8d603f9442ec
Darvin Alexandr <zelenij24@yandex.ru>
2022-09-23 13:48:30 +0300

    added tests constructor

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,97 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+ class HorseTest {
+
+    @Test
+    void checkIllegalArgumentException() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            throw new IllegalArgumentException();
+        });
+    }
+
+    @Test
+    void checkIllegalArgumentExceptionMessage() {
+        String expectedMessage = "Name cannot be null.";
+        Throwable exception = assertThrows(IllegalArgumentException.class, ()-> {
+                    throw new IllegalArgumentException(expectedMessage);
+                });
+
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void checkFirstParamIsBlank(String name) {
+        if (name.isBlank()) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                throw new IllegalArgumentException();
+            });
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void checkFirstParamBlankMessage(String name) {
+        String expectedMessage = "Name cannot be blank.";
+
+        if (name.isBlank()) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                        throw new IllegalArgumentException(expectedMessage);
+                    });
+
+            assertEquals(expectedMessage, exception.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.0})
+    void checkSecondParamMinus(double speed) {
+        if (speed < 0) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                throw new IllegalArgumentException();
+            });
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.0})
+    void checkSecondParamMinusMessage(double speed) {
+        String expectedMessage = "Speed cannot be negative.";
+
+        if (speed < 0) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                        throw new IllegalArgumentException(expectedMessage);
+                    });
+
+            assertEquals(expectedMessage, exception.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.0})
+    void checkThirdParamMinus(double distance) {
+        if (distance < 0) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                throw new IllegalArgumentException();
+            });
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {-1.0})
+    void checkThirdParamMinusMessage(double distance) {
+        String expectedMessage = "Distance cannot be negative.";
+
+        if (distance < 0) {
+            Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+                        throw new IllegalArgumentException(expectedMessage);
+                    });
+
+            assertEquals(expectedMessage, exception.getMessage());
+        }
+    }
+}
