1fa70e2540926b7803c2465d8e08bb85133da1f0
Bering-l <125829687+Bering-l@users.noreply.github.com>
2023-05-27 19:55:35 +0300

    final version

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -2,6 +2,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.util.ArrayList;
