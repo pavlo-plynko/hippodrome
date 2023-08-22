f6316158bb02140c036034c232d69e4ae0197aaa
Alexey Pogonin <skulkyr20@gmail.com>
2023-08-22 21:18:16 +0300

    Add all tests.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,10 +1,25 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
+    @Disabled
+    @Timeout(22)
     void main() {
+        PrintStream origin = System.out;
+        try {
+            System.setOut(new PrintStream(File.createTempFile("temp", ".tmp")));
+            Main.main(new String[]{});
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.setOut(origin);
+        }
     }
 }
