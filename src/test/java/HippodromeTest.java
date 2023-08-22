6ebaa668f424970c2e667a155b4f477d8111a034
Alexey Pogonin <skulkyr20@gmail.com>
2023-08-22 21:20:04 +0300

    Remove import

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,11 +1,7 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
